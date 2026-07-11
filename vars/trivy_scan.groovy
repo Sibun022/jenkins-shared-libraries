ea38fe9bbc85a0e52c8560c88695a2c209d9d207
Sibun022 <141263330+Sibun022@users.noreply.github.com>
2026-07-11 20:50:27 +0530

    Enhance Trivy scan with severity and output format

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,3 +1,9 @@
-def call(){
-  sh "trivy fs ."
+def call() {
+    echo "Running Trivy Filesystem Scan"
+
+    sh '''
+        trivy fs . \
+        --format table \
+        --severity HIGH,CRITICAL
+    '''
 }
