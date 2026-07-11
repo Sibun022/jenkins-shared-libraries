bc1b3d1e1480a9acb15fc843376d8733e58bff6b
Sibun022 <141263330+Sibun022@users.noreply.github.com>
2026-07-11 20:51:05 +0530

    Add OWASP Dependency Check pipeline step

---
+++ vars/owasp_dependency.groovy
@@ -0,0 +1,13 @@
+def call() {
+
+    echo "Running OWASP Dependency Check"
+
+    dependencyCheck additionalArguments: '''
+        --scan .
+        --format HTML
+        --format XML
+    ''',
+    odcInstallation: 'OWASP'
+
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+}
